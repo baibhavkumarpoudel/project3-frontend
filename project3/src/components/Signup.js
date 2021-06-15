3e900dc9b90f380b279c29c049b801589401da40
Skyler Vassallo <Skyler_Vassallo@glic.com>
2021-06-15 16:53:52 -0400

    Created Signup + Home page

---
+++ project3/src/components/Signup.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+
+class Signup extends Component {
+    constructor() {
+        super()
+        this.state= {
+            data: {
+                username: "",
+                password: "",
+                name: "",
+                division: "",
+                img: "",
+            }
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>Sign Up</h1>
+                <form>
+                    <input type="text" name="username" placeholder="Username"/><br></br>
+                    <input type="password" name="password" placeholder="Password"/><br></br>
+                    <input type="text" name="name" placeholder="Team Name"/><br></br>
+                    <input type="text" name="division" placeholder="Division"/><br></br>
+                    <input type="text" name="img" placeholder="Photo"/><br></br>
+                    <h2>Pick your Primary Team Color</h2>
+                    <input type="color" name="color1" placeholder="Primary Team Color"/><br></br>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Signup;
