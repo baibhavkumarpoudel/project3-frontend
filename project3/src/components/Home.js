4d7cf52e854a48a41faa3c57b575d3425572e608
Baibhav K Poudel <bkpoudel@up.com>
2021-06-17 14:48:22 -0500

    worked on css styling and axios errors

--- project3/src/components/Home.js
+++ project3/src/components/Home.js
@@ -3,16 +3,22 @@ import {Link} from "react-router-dom";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Sunday Amateur Soccer League</h1>
+        <div className="home" >
+            <h1 className="home-h1">Sunday Amateur Soccer League</h1>
             <nav>
                 <Link to="/login">Log In</Link><br></br>
                 <Link to="/signup">Sign Up</Link>
             </nav>
+           
+            <div className="home-intro">
             <p>Welcome to the Sunday Leagues, the official website for the nationwide phenomenon of Amateur level Soccer Competition.</p>
             <p>Please sign up with your team and get started!</p>
+            </div>
+           
         </div>
     )
 }
 
 export default Home;
+
+
