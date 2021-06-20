f147555314cff7f92fa4d9d75f5e5a8f2f417e1a
Skyler Vassallo <Skyler_Vassallo@glic.com>
2021-06-20 17:34:46 -0400

    Updated CSS and formatting for Profile

--- project3/src/components/Profile.js
+++ project3/src/components/Profile.js
@@ -8,56 +8,39 @@ class Profile extends Component {
         this.state = {
             team: {},
             data: {
-<<<<<<< HEAD
-                name: "",
-=======
-                name: "Hello"
->>>>>>> f83b2da9246920aa7116e1a5449c942fcef2a968
+                name: "Your Team Name",
+                division: "Your Division",
+                img: "../src/images/home.jpeg"
             }
         }
     }
 
-<<<<<<< HEAD
-    componentDidMount = () => {
-        axios.get(`http://localhost:3001/profile/${this.props.match.params.id}`)
-        .then(resp => {
-            this.setState({  
-=======
     componentDidMount= () => {
         axios.get(`http://localhost:3001/profile/${this.props.match.params.id}`)
         .then(resp => {
             this.setState({
->>>>>>> f83b2da9246920aa7116e1a5449c942fcef2a968
                 team: resp.data
             })
         })
     }
 
-<<<<<<< HEAD
-    render () {
-        console.log(this.state.team)
-        const team = this.state.team
-=======
     
 
     render () {
         console.log(this.state.data)
-        
         const team = this.state.data
->>>>>>> f83b2da9246920aa7116e1a5449c942fcef2a968
         return (
-            <div>
-                <img src="" />
-                <h2>{team.name}</h2>
-<<<<<<< HEAD
-                <h3>Divisin</h3>
-=======
-                <h3>Division</h3>
->>>>>>> f83b2da9246920aa7116e1a5449c942fcef2a968
+            <div className="profile">
+                <header className="profile-header">
+                    <h2>{team.name}</h2>
+                    <h3>{team.division}</h3>
+                </header>
                 {/* Link to Player Page to add players directly to team signed in on */}
-                <Link to="/player">Add a Player</Link><br></br>
-                <p>Welcome to the Sunday Leagues, the official website for the nationwide phenomenon of Amateur level Soccer Competition.</p>
-                <p>Please sign up with your team and get started!</p>
+                <main className="profile-main">
+                    <img src={team.img} />
+                    <Link to="/player">Add a Player</Link>
+                    <p>This is the list of active players.</p>
+                </main>
             </div>
         )
     }
